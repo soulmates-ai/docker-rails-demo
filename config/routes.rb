823c644a251597c7489aa388b5d9679ad55b4e5e
Dirk de Kok <dirk@mobtest.com>
2018-08-22 23:22:07 -0700

    custom homepage using Webpacker

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root 'welcome#index'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
